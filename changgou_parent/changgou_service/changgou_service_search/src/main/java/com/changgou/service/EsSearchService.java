3bf9c99ab5703fd28b70e2426d9420915e2b3c96
jiangnanhuabei <13343257639@163.com>
2020-11-03 17:54:04 +0800

    elasticsearch 模糊搜索

--- changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/service/EsSearchService.java
+++ changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/service/EsSearchService.java
@@ -3,11 +3,10 @@ package com.changgou.service;
 import java.util.Map;
 
 public interface EsSearchService {
-
     /**
      * ES的综合搜索
      * @param searchMap
      * @return
      */
-    Map search(Map<String,String> searchMap);
+    Map search(Map<String, String> searchMap);
 }
